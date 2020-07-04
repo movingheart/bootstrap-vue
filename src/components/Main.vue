fd0589fbca65c2a122e66fd9131da791e47d11a7
movingheart <movingheart000@gmail.com>
2020-07-04 18:33:34 +0800

    使用bootstrap4样式

---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+            <div class="row">
+                <h1>请输入用户名搜索</h1>
+            </div>
+            <div class="row">
+                <ul class="list-inline w-100">
+                    <div class="row">
+
+                        <li class="col-md-3 col-6">
+                            结果1
+                        </li>
+                        <li class="col-md-3 col-6">
+                            结果2
+                        </li>
+                        <li class="col-md-3 col-6">
+                            结果3
+                        </li>
+                        <li class="col-md-3 col-6">
+                            结果4
+                        </li>
+                        <li class="col-md-3 col-6">
+                            结果5
+                        </li>
+                    </div>
+                </ul>
+            </div>
+
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "Main"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="jumbotron">
+            <div>
+                <form>
+                    <label for="exampleUsername">Search Github Users</label>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="exampleUsername">
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <button type="submit" class="btn border">提交</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        name: "Search"
+    }
+</script>
+
+<style scoped>
+
+</style>
